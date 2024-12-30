183d908af8e59c1ab194a54115ebbafbfae43c43
soham-Jain08 <sohamkholapure2005@gmail.com>
2024-12-30 23:37:53 +0530

    Add files via upload

---
+++ site.js
@@ -0,0 +1,36 @@
+const menuOpenButton = document.querySelector("#menu-open-button");
+
+menuOpenButton.addEventListener("click",() =>{
+    document.body.classList.toggle("show-mobile-menu");
+});
+
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuCloseButton.addEventListener("click",()=> menuOpenButton.click());
+
+const swiper = new Swiper('.slider-wrapper',{
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 25,
+
+    pagination:{
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+
+    navigation:{
+        nextEl: '.swiper-button-next',
+        prevEl:'swiper-button-prev',
+    }, 
+
+    breakpoints:{
+        0:{
+            slidesPerView: 1},
+        768:{
+            slidesPerView: 2},
+        1024:{
+            slidesPerView: 3},
+
+    }
+})
